46b799d293ed30c63c9adcf0353d69b12b0b350b
Joan2509 <wjoanmumbi@gmail.com>
2024-12-11 23:10:45 +0300

    feat: declared structs for search suggestions and results

--- server/models.go
+++ server/models.go
@@ -25,6 +25,17 @@ type Relation struct {
 	DatesLocation map[string][]string `json:"datesLocations"`
 }
 
+type SearchResult struct {
+	Artist    Artist
+	MatchType []string
+	Relevance float64
+}
+
+type SearchSuggestion struct {
+	Value string // The actual suggestion text
+	Type  string // Type of suggestion (artist, member, location, etc.)
+}
+
 // Passes dynamic data to HTML templates for rendering web pages.
 type TemplateData struct {
 	Title     string
